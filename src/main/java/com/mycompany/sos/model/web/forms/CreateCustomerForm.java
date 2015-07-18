e87e997e43678527da5ed92b2aef8cd9a1fcbfbc
Colin But <colinbut@users.noreply.github.com>
2015-07-18 21:22:20 +0100

    added ViewModel form backing model object for when creating customer

--- src/main/java/com/mycompany/sos/model/web/forms/CreateCustomerForm.java
+++ src/main/java/com/mycompany/sos/model/web/forms/CreateCustomerForm.java
@@ -3,10 +3,76 @@
  */
 package com.mycompany.sos.model.web.forms;
 
+import java.util.Date;
+
 /**
  * @author colin
  *
  */
 public class CreateCustomerForm {
 
+	private String firstName;
+	private String lastName;
+	private Date dateOfBirth;
+	private String emailAddress;
+	private int houseFlatNo;
+	private String street;
+	private String postcode;
+	private String city;
+	private String country;
+	
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public Date getDateOfBirth() {
+		return dateOfBirth;
+	}
+	public void setDateOfBirth(Date dateOfBirth) {
+		this.dateOfBirth = dateOfBirth;
+	}
+	public String getEmailAddress() {
+		return emailAddress;
+	}
+	public void setEmailAddress(String emailAddress) {
+		this.emailAddress = emailAddress;
+	}
+	public int getHouseFlatNo() {
+		return houseFlatNo;
+	}
+	public void setHouseFlatNo(int houseFlatNo) {
+		this.houseFlatNo = houseFlatNo;
+	}
+	public String getStreet() {
+		return street;
+	}
+	public void setStreet(String street) {
+		this.street = street;
+	}
+	public String getPostcode() {
+		return postcode;
+	}
+	public void setPostcode(String postcode) {
+		this.postcode = postcode;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getCountry() {
+		return country;
+	}
+	public void setCountry(String country) {
+		this.country = country;
+	}
 }
