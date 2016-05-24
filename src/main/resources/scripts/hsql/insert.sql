0fbd41d128ade6f93bdb6e39606aa83c38617376
Colin But <colinbut@users.noreply.github.com>
2016-05-24 21:04:04 +0100

    sql scripts

---
+++ src/main/resources/scripts/hsql/insert.sql
@@ -0,0 +1,18 @@
+insert into address (city, country, house_flat_no, postcode, street)
+values ('London', 'UK', 55, 'NH35HT', 'Madison Square');
+
+-- add customers
+
+-- add customer payment details
+
+insert into items (item_name, item_price) values ('test-item1',23.2);
+insert into items (item_name, item_price) values ('test-item2',25.2);
+insert into items (item_name, item_price) values ('test-item3',12.2);
+insert into items (item_name, item_price) values ('test-item4',67.2);
+insert into items (item_name, item_price) values ('test-item5',34.2);
+insert into items (item_name, item_price) values ('test-item6',09.2);
+insert into items (item_name, item_price) values ('test-item7',18.2);
+
+-- add orders
+
+-- link orders-items
