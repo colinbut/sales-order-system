d8e98368c91686695b0e02433fefddeb38e031ff
Colin But <colinbut@users.noreply.github.com>
2016-06-11 16:56:14 +0100

    new unit test for CustomerController

--- online-application/src/test/java/com/mycompany/sos/web/CustomerControllerTest.java
+++ online-application/src/test/java/com/mycompany/sos/web/CustomerControllerTest.java
@@ -1,21 +1,72 @@
 /*
  * |-------------------------------------------------
- * | Copyright © 2015 Colin But. All rights reserved. 
+ * | Copyright © 2015 Colin But. All rights reserved.
  * |-------------------------------------------------
  */
 package com.mycompany.sos.web;
 
 import com.mycompany.sos.UnitTestCategory;
+import com.mycompany.sos.model.Customer;
+import com.mycompany.sos.model.CustomerPaymentDetail;
+import com.mycompany.sos.service.CustomerService;
+import org.junit.Before;
+import org.junit.Test;
 import org.junit.experimental.categories.Category;
+import org.mockito.InjectMocks;
+import org.mockito.Matchers;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.springframework.ui.ModelMap;
+import org.springframework.validation.BeanPropertyBindingResult;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.DirectFieldBindingResult;
+import org.springframework.web.servlet.ModelAndView;
+
+import static org.junit.Assert.assertEquals;
 
 /**
  * {@link CustomerControllerTest} test class
- * 
- * @author colin
  *
+ * @author colin
  */
 @Category(UnitTestCategory.class)
 public class CustomerControllerTest {
 
-	
+    @Mock
+    private CustomerService customerService;
+
+    @InjectMocks
+    private CustomerController customerController = new CustomerController();
+
+    @Before
+    public void setup() {
+        MockitoAnnotations.initMocks(this);
+    }
+
+
+    @Test
+    public void testListCustomers() {
+        assertEquals("customers/customers", customerController.listCustomers(new ModelMap()));
+    }
+
+    @Test
+    public void testShowCreateCustomerFormPage() {
+        assertEquals("customers/customers-createCustomer", customerController.showCreateCustomerFormPage(new ModelMap()));
+    }
+
+    @Test
+    public void testCreateCustomer() {
+        Customer customer = new Customer();
+        customer.setCustomerId(1);
+        customer.setCustomerPaymentDetail(new CustomerPaymentDetail());
+
+        Mockito.when(customerService.addCustomer(Matchers.any(Customer.class)))
+            .thenReturn(customer);
+
+        ModelAndView modelAndView = customerController.createCustomer(customer, new DirectFieldBindingResult(null, ""));
+        assertEquals("redirect:/customers/1", modelAndView.getViewName());
+    }
+
+
 }
--- online-application/src/test/java/com/mycompany/sos/web/ItemControllerTest.java
+++ online-application/src/test/java/com/mycompany/sos/web/ItemControllerTest.java
@@ -1,6 +1,6 @@
 /*
  * |-------------------------------------------------
- * | Copyright © 2015 Colin But. All rights reserved. 
+ * | Copyright © 2015 Colin But. All rights reserved.
  * |-------------------------------------------------
  */
 package com.mycompany.sos.web;
@@ -20,40 +20,39 @@ import static org.junit.Assert.*;
 
 /**
  * {@link ItemControllerTest} test class
- * 
- * @author colin
  *
+ * @author colin
  */
 @Category(UnitTestCategory.class)
 public class ItemControllerTest {
 
 	@Mock
 	private ItemService itemService;
-	
+
 	@InjectMocks
 	private ItemController itemController = new ItemController();
-	
+
 	@Before
 	public void setup() {
 		MockitoAnnotations.initMocks(this);
 	}
-	
+
 	@Test
 	public void testShowItemsList() {
 		assertEquals("items", itemController.showItemsList(new ModelMap()));
 	}
-	
+
 	@Test
 	public void testShowCreateItemPage() {
 		assertEquals("items-create", itemController.showCreateItemPage(new ModelMap()));
 	}
-	
+
 	@Test
 	public void testCreateItem() {
 		Mockito.when(itemService.addItem(Matchers.any(Item.class))).thenReturn(true);
 		ModelAndView modelAndView = itemController.createItem(new Item(),
 				new DirectFieldBindingResult(null, null));
-		
+
 		assertNotNull(modelAndView);
 		assertEquals("redirect:/items", modelAndView.getViewName());
 	}
