a27f5e5a3755758b59c3640514935655a1a59680
Colin But <colinbut@users.noreply.github.com>
2016-06-11 16:35:51 +0100

    added address embedded object to script

--- online-application/src/main/resources/scripts/mongodb/mongodb.js
+++ online-application/src/main/resources/scripts/mongodb/mongodb.js
@@ -8,5 +8,8 @@ db.customers.insert({
     firstname : 'firstname',
     lastname : 'lastname',
     dob : '2000-12-21 00:00:00',
-    email : 'firstname.lastname@email.com'
+    email : 'firstname.lastname@email.com',
+    address : [
+        { flathouseno : 50, street: "streetname", city : "London", country : "England", postcode : "SO24 9NF" }
+    ]
 })
