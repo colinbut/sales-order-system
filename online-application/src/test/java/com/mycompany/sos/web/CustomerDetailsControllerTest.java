430047854182a5de6764971ecddd0ac0faebeded
Colin But <colinbut@users.noreply.github.com>
2016-06-12 15:28:26 +0100

    CustomerDetailsController Unit Test

--- online-application/src/test/java/com/mycompany/sos/web/CustomerDetailsControllerTest.java
+++ online-application/src/test/java/com/mycompany/sos/web/CustomerDetailsControllerTest.java
@@ -5,5 +5,46 @@
  */
 package com.mycompany.sos.web;
 
+import com.mycompany.sos.UnitTestCategory;
+import com.mycompany.sos.model.Customer;
+import com.mycompany.sos.service.CustomerService;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+import org.mockito.InjectMocks;
+import org.mockito.Matchers;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.springframework.validation.support.BindingAwareModelMap;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * {@link CustomerDetailsControllerTest} test class
+ *
+ * @author colin
+ */
+@Category(UnitTestCategory.class)
 public class CustomerDetailsControllerTest {
+
+    @Mock
+    private CustomerService customerService;
+
+    @InjectMocks
+    private CustomerDetailsController customerDetailsController = new CustomerDetailsController();
+
+
+    @Before
+    public void setup() {
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    public void testGetCustomerDetailsPage() {
+        Mockito.when(customerService.findCustomerByCustomerId(Matchers.anyInt()))
+            .thenReturn(new Customer());
+        assertEquals("customers/customerDetails", customerDetailsController.getCustomerDetailsPage("1", new BindingAwareModelMap()));
+    }
+
 }
