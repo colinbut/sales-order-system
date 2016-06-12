b61bcb7ea98a4cee1353ab9e075c65534d6c9a46
Colin But <colinbut@users.noreply.github.com>
2016-06-12 14:49:21 +0100

    embedded object for customer payment details to mongodb script

--- online-application/src/main/resources/scripts/mongodb/mongodb.js
+++ online-application/src/main/resources/scripts/mongodb/mongodb.js
@@ -11,7 +11,12 @@ db.customers.insert({
     email : 'firstname1.lastname1@email.com',
     address : [
         { flathouseno : 50, street: "Rodney Street", city : "London", country : "UK", postcode : "HD4 5HB" }
-    ]
+    ],
+    customerPaymentDetails : {
+        cardExpiryDate : '2015-12-31 00:00:00',
+        cardNumber: '1234567812345678',
+        customerReference : 'customer-ref-1'
+    }
 })
 
 db.customers.insert({
@@ -21,7 +26,12 @@ db.customers.insert({
     email : 'firstname2.lastname2@email.com',
     address : [
         { flathouseno : 23, street: "Albert Square", city : "Manchester", country : "UK", postcode : "SO24 9NF" }
-    ]
+    ],
+    customerPaymentDetails : {
+        cardExpiryDate : '2015-12-31 00:00:00',
+        cardNumber: '4024007135995586',
+        customerReference : 'customer-ref-2'
+    }
 })
 
 db.customers.insert({
@@ -31,7 +41,12 @@ db.customers.insert({
     email : 'firstname3.lastname3@email.com',
     address : [
         { flathouseno : 556, street: "Tomson Road", city : "Leeds", country : "England", postcode : "SA15 5NW" }
-    ]
+    ],
+    customerPaymentDetails : {
+        cardExpiryDate : '2015-12-31 00:00:00',
+        cardNumber: '4532754928070797',
+        customerReference : 'customer-ref-3'
+    }
 })
 
 db.customers.insert({
@@ -41,7 +56,12 @@ db.customers.insert({
     email : 'firstname4.lastname4@email.com',
     address : [
         { flathouseno : 128, street: "Milestone Alley", city : "Birmingham", country : "England", postcode : "DL8 3AR" }
-    ]
+    ],
+    customerPaymentDetails : {
+        cardExpiryDate : '2015-12-31 00:00:00',
+        cardNumber: '4532790030107846',
+        customerReference : 'customer-ref-4'
+    }
 })
 
 db.customers.insert({
@@ -51,7 +71,12 @@ db.customers.insert({
     email : 'firstname5.lastname5@email.com',
     address : [
         { flathouseno : 16, street: "Cheap Lane", city : "Glasgow", country : "England", postcode : "LL63 5EY" }
-    ]
+    ],
+    customerPaymentDetails : {
+        cardExpiryDate : '2015-12-31 00:00:00',
+        cardNumber: '4929126881917390',
+        customerReference : 'customer-ref-5'
+    }
 })
 
 db.customers.insert({
@@ -61,7 +86,12 @@ db.customers.insert({
     email : 'firstname6.lastname6@email.com',
     address : [
         { flathouseno : 8, street: "Fife Crescent", city : "Newcastle", country : "UK", postcode : "WS4 1LP" }
-    ]
+    ],
+    customerPaymentDetails : {
+        cardExpiryDate : '2015-12-31 00:00:00',
+        cardNumber: '4929337860089115',
+        customerReference : 'customer-ref-6'
+    }
 })
 
 db.customers.insert({
@@ -71,7 +101,12 @@ db.customers.insert({
     email : 'firstname7.lastname7@email.com',
     address : [
         { flathouseno : 3972, street: "Winston Park", city : "Cardiff", country : "UK", postcode : "LL28 5BG" }
-    ]
+    ],
+    customerPaymentDetails : {
+        cardExpiryDate : '2015-12-31 00:00:00',
+        cardNumber: '5567638144721645',
+        customerReference : 'customer-ref-7'
+    }
 })
 
 db.customers.insert({
@@ -81,7 +116,12 @@ db.customers.insert({
     email : 'firstname8.lastname8@email.com',
     address : [
         { flathouseno : 1, street: "Victoria Place", city : "Edinburgh", country : "UK", postcode : "G32 0LG" }
-    ]
+    ],
+    customerPaymentDetails : {
+        cardExpiryDate : '2015-12-31 00:00:00',
+        cardNumber: '5512296550494052',
+        customerReference : 'customer-ref-8'
+    }
 })
 
 db.customers.insert({
@@ -91,7 +131,12 @@ db.customers.insert({
     email : 'firstname9.lastname9@email.com',
     address : [
         { flathouseno : 12, street: "Storm Gate", city : "Derby", country : "UK", postcode : "SK13 2BA" }
-    ]
+    ],
+    customerPaymentDetails : {
+        cardExpiryDate : '2015-12-31 00:00:00',
+        cardNumber: '5214682212584722',
+        customerReference : 'customer-ref-9'
+    }
 })
 
 db.customers.insert({
@@ -101,5 +146,10 @@ db.customers.insert({
     email : 'firstname10.lastname10@email.com',
     address : [
         { flathouseno : 32, street: "Hung Row", city : "Sheffield", country : "UK", postcode : "PL32 9QG" }
-    ]
+    ],
+    customerPaymentDetails : {
+        cardExpiryDate : '2015-12-31 00:00:00',
+        cardNumber: '5467916103505618',
+        customerReference : 'customer-ref-10'
+    }
 })
