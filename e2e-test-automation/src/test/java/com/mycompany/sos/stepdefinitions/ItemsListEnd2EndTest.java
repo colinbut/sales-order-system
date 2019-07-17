2b8e387962a812a9bceb1ea3cee25ad3f51cf2b9
Colin But <colinbut@users.noreply.github.com>
2019-07-18 00:16:24 +0100

    code formatting

--- e2e-test-automation/src/test/java/com/mycompany/sos/stepdefinitions/ItemsListEnd2EndTest.java
+++ e2e-test-automation/src/test/java/com/mycompany/sos/stepdefinitions/ItemsListEnd2EndTest.java
@@ -1,6 +1,6 @@
 /*
  * |-------------------------------------------------
- * | Copyright © 2015 Colin But. All rights reserved. 
+ * | Copyright © 2015 Colin But. All rights reserved.
  * |-------------------------------------------------
  */
 package com.mycompany.sos.stepdefinitions;
@@ -17,31 +17,28 @@ import static org.junit.Assert.*;
 
 /**
  * {@link ItemsListEnd2EndTest} test class
- * 
- * @author colin
  *
+ * @author colin
  */
-//@Category(End2EndCategory.class)
+@Category(End2EndCategory.class)
 public class ItemsListEnd2EndTest {
 
-	ItemController itemController = new ItemController();
-	
-	String viewName;
-	
-	@Given("^i am on the customers list page$")
-	public void i_am_on_the_customers_list_page() throws Throwable {
-	    //
-	}
+    ItemController itemController = new ItemController();
 
-	@When("^i click on the 'items' tab$")
-	public void i_click_on_the_items_tab() throws Throwable {
-	    viewName = itemController.showItemsList(new ModelMap());
-	}
+    String viewName;
 
-	@Then("^i should be presented with the items list page$")
-	public void i_should_be_presented_with_the_items_list_page() throws Throwable {
-	    assertEquals(viewName, "items");
-	}
-	
-	
+    @Given("^i am on the customers list page$")
+    public void i_am_on_the_customers_list_page() throws Throwable {
+        //
+    }
+
+    @When("^i click on the 'items' tab$")
+    public void i_click_on_the_items_tab() throws Throwable {
+        viewName = itemController.showItemsList(new ModelMap());
+    }
+
+    @Then("^i should be presented with the items list page$")
+    public void i_should_be_presented_with_the_items_list_page() throws Throwable {
+        assertEquals(viewName, "items");
+    }
 }
